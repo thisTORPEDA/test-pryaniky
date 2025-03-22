dead37938a246855ca09b7e8206dcac6b9ae894b
TORPEDA <link.4el@gmail.com>
2025-03-22 15:52:11 +0500

    README

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "https://test.v5.pryaniky.com" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://test.v5.pryaniky.com" }), // Лучше всего кончено вынести в env
   tagTypes: ["table"],
   endpoints: () => ({}),
 });
